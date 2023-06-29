44643a16fdae8e7156c928cca20f52af50897c04
minyoung <ericashin1@g.skku.edu>
2023-06-29 20:35:01 +0900

    pagers

--- app/src/main/java/com/example/sadcamp/MyViewPagerAdapter.java
+++ app/src/main/java/com/example/sadcamp/MyViewPagerAdapter.java
@@ -1,4 +1,31 @@
 package com.example.sadcamp;
 
-public class MyViewPagerAdapter {
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.example.sadcamp.fragments.CallFragment;
+import com.example.sadcamp.fragments.FreeFragment;
+import com.example.sadcamp.fragments.GallaryFragment;
+
+public class MyViewPagerAdapter extends FragmentStateAdapter {
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position){
+            case 0:
+                return new CallFragment();
+            case 1:
+                return new GallaryFragment();
+            case 2:
+                return new FreeFragment();
+            default:
+                return new CallFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
 }
