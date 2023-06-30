83774ff1f1169f17116ef6b64a7718856f7bc9c6
minyoung <ericashin1@g.skku.edu>
2023-06-30 16:35:42 +0900

    gallery fragment

--- app/src/main/java/com/example/sadcamp/GallaryAdapter.java
+++ app/src/main/java/com/example/sadcamp/GallaryAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 
 public class GallaryAdapter extends BaseAdapter {
 
@@ -13,6 +15,8 @@ public class GallaryAdapter extends BaseAdapter {
             R.drawable.a,R.drawable.b,R.drawable.c,
             R.drawable.d,R.drawable.e
     };
+
+
 
     public GallaryAdapter(Context mContext){
         this.mContext = mContext;
@@ -24,17 +28,24 @@ public class GallaryAdapter extends BaseAdapter {
     }
 
     @Override
-    public Object getItem(int position){
-        return imageArray[position];
+    public Object getItem(int i){
+        return imageArray[i];
     }
 
     @Override
     public long getItemId(int i) {
+
         return 0;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        ImageView imageView = new ImageView(mContext);
+        imageView.setImageResource(imageArray[i]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setLayoutParams(new ViewGroup.LayoutParams( 340, 350));
+
+        return imageView;
     }
+
 }
