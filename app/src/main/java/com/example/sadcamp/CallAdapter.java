cd065382471ff93c43dc144bbbbdb71e7820f505
minyoung <ericashin1@g.skku.edu>
2023-07-02 20:32:45 +0900

    TitleActivity

--- app/src/main/java/com/example/sadcamp/CallAdapter.java
+++ app/src/main/java/com/example/sadcamp/CallAdapter.java
@@ -6,8 +6,12 @@ import android.view.ContentInfo;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.app.AlertDialog;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.google.gson.Gson;
+
+
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -75,13 +79,33 @@ public class CallAdapter extends RecyclerView.Adapter<CallAdapter.ViewHolder> {
     // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
-
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
         viewHolder.getNameView().setText(contactList.get(position).getName());
         viewHolder.getEmailView().setText(contactList.get(position).getMail());
         viewHolder.getImgView().setImageResource(contactList.get(position).getPic());
+
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pos = viewHolder.getAdapterPosition();
+                if (pos != RecyclerView.NO_POSITION) {
+                    ContactData contactData = contactList.get(pos); // 'position'을 'pos'로 변경
+                    Gson gson = new Gson();
+                    String jsonContactData = gson.toJson(contactData);
+
+                    // Create and show a Dialog with the JSON contact data
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setTitle("Contact Information")
+                            .setMessage(jsonContactData)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            }
+        });
     }
+
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
